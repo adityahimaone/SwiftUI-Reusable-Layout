81a86386a7c09c8d3d1f83bf00548f15059c6e9c
adityahimaone <adityahimaone@gmail.com>
2024-01-18 22:20:35 +0700

    Initial Commit

---
+++ StackLayoutFinal/StackLayoutFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackLayoutFinalApp.swift
+//  StackLayoutFinal
+//
+//  Created by Aditya Himawan on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct StackLayoutFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
